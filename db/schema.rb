20d1f493b5c1e8cc341fe90fd6bec5616f9699b1
Chris Voxland <chrisvoxland@gmail.com>
2015-03-24 21:57:37 -0400

    add event id to event model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150319022836) do
+ActiveRecord::Schema.define(version: 20150325012458) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 20150319022836) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "calendar_id"
+    t.string   "event_id"
   end
 
   add_index "events", ["calendar_id"], name: "index_events_on_calendar_id", using: :btree
